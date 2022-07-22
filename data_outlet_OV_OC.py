ff650f4949892e8722578452a4da2f71265f00de
scmpamekasan <109006899+scmpamekasan@users.noreply.github.com>
2022-07-22 17:28:01 +0700

    Update data_outlet_OV_OC.py

--- data_outlet_OV_OC.py
+++ data_outlet_OV_OC.py
@@ -3,6 +3,8 @@ import pandas as pd
 import streamlit as st  # pip install streamlit
 import streamlit.components.v1 as components
 import numpy as np
+from openpyxl import Workbook
+
 
 st.set_page_config(layout="wide")
 
