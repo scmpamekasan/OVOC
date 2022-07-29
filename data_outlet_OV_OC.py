5c142871fa93e0e9583f94b9e110557bcae9c2d1
scmpamekasan <109006899+scmpamekasan@users.noreply.github.com>
2022-07-29 10:41:21 +0700

    Update data_outlet_OV_OC.py

--- data_outlet_OV_OC.py
+++ data_outlet_OV_OC.py
@@ -21,38 +21,33 @@ df_OC_OV =  pd.read_excel(
 
 
 
-df_OC_OV_1 = df_OC_OV.drop(['DSO Name','Daerah','Zona','LastTransaction(OC)'], axis=1)
-df_OC_OV_3 = df_OC_OV.drop(['DSO Name','Daerah','Zona','LastVisit(OV)'], axis=1)
+df_OC_OV_1 = df_OC_OV.drop(['DSO Name','Daerah','Zona','LastTransaction (OC)'], axis=1)
+df_OC_OV_3 = df_OC_OV.drop(['DSO Name','Daerah','Zona','LastVisit (OV)'], axis=1)
 
-df_OC_OV_2 = df_OC_OV_1[df_OC_OV_1['LastVisit(OV)'].isnull()]
-df_OC_OV_4 = df_OC_OV_3[df_OC_OV_3['LastTransaction(OC)'].isnull()]
+
+df_OC_OV_2 = df_OC_OV_1[df_OC_OV_1['LastVisit (OV)'].isnull()]
+df_OC_OV_4 = df_OC_OV_3[df_OC_OV_3['LastTransaction (OC)'].isnull()]
 
 
 
 
 sektor1 = (df_OC_OV_2.loc[:,("Sektor")].drop_duplicates())
 list_sektor1 = sektor1.values.tolist()
-
 sektor2 = (df_OC_OV_4.loc[:,("Sektor")].drop_duplicates())
 list_sektor2 = sektor2.values.tolist()
 
-Sektor = st.selectbox("Pilih Sektor", list_sektor1)
-st.write("Update Per 16 Juli 2022 (data akan diupdate tiap minggu)")
-
-#
 col1,col2 = st.columns(2)
 with col1 :
     st.subheader ('Outlet Belum ⚽️🅥')
-    
+    Sektor = st.selectbox("Pilih Sektor", list_sektor1)
+
     df_selection = df_OC_OV_2.query("Sektor==@Sektor" )
     st.write('Jumlah Outlet Belum Visit=',len(df_selection.index))
     st.write(df_selection)
 
 with col2 :
     st.subheader ('Outlet Belum ⚽️🅲')
- 
 
-    df_selection2 = df_OC_OV_4.query("Sektor==@Sektor" )
-    st.write('Jumlah Outlet Belum Transaksi=',len(df_selection2.index))
-    st.write(df_selection2)
-#
+    df_selection = df_OC_OV_4.query("Sektor==@Sektor" )
+    st.write('Jumlah Outlet Belum Transaksi=',len(df_selection.index))
+    st.write(df_selection)
